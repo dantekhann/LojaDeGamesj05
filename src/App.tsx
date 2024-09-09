a199b33817db2a043b810f3ca815f470f806d188
DanteKhan <paulo.dante.neto@gmail.com>
2024-09-09 10:44:13 -0300

    Carrossel

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import DeletarCategoria from './components/categorias/deletarcategorias/DeletarCategoria';
+import FormCategoria from './components/categorias/formcategoria/FormCategoria';
+import ListarCategorias from './components/categorias/listarcategorias/ListarCategorias';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import DeletarProduto from './components/produtos/deletarprodutos/DeletarProduto';
+import FormularioProduto from './components/produtos/formproduto/FormularioProduto';
+import ListarProdutos from './components/produtos/listarprodutos/ListarProdutos';
+import { ToastContainer } from 'react-toastify';
+// import { AuthProvider } from './contexts/AuthContext';
+import Cart from './components/carrinho/cart/Cart';
+import { CartProvider } from './contexts/CartContext';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+
+  return (
+    <>
+      {/* <AuthProvider> */}
+        <CartProvider>
+          <ToastContainer />
+          <BrowserRouter>
+            <Navbar />
+            <div className='min-h-[90vh] bg-gray-200'>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/categorias" element={<ListarCategorias />} />
+                <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+                <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+                <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+                <Route path="/produtos" element={<ListarProdutos />} />
+                <Route path="/cadastrarproduto" element={<FormularioProduto />} />
+                <Route path="/editarproduto/:id" element={<FormularioProduto />} />
+                <Route path="/deletarproduto/:id" element={<DeletarProduto />} />
+                <Route path="/cart" element={<Cart />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </CartProvider>
+      {/* </AuthProvider> */}
+    </>
+  );
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+﻿import Carrossel from "../../components/carrossel/Carrosel"
+import ListarProdutos from "../../components/produtos/listarprodutos/ListarProdutos"
+
+function Home() {
+    return (
+        <>
+            <div>
+                <Carrossel />
+            </div>
+            <ListarProdutos />
+        </>
+    )
+}
+
+export default Home
